891dd612033e615fd7c26074e6bbdac22e480348
Sebastian Jeong <sebastianrcnt@gmail.com>
2021-04-06 21:21:04 +0900

    init branch lx

--- server/app.py
+++ server/app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
   return render_template('pages/home.html')
+
+@app.route("/keywords")
+def get_keywords_page():
+  return render_template("pages/keywords.html")
