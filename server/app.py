b4cb66f3d915de7def2b917cf6ec0b9090645d81
Sebastian Jeong <sebastianrcnt@gmail.com>
2021-04-11 16:06:32 +0900

    save

--- server/app.py
+++ server/app.py
@@ -2,10 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-  return render_template('pages/home.html')
+    return render_template('pages/home.html')
+
 
 @app.route('/feature1')
-def feature():
-  return render_template("pages/feature1.html")
+def feature1():
+    return render_template("pages/feature1.html")
+
+
+@app.route('/feature2')
+def feature2():
+    return render_template("pages/feature2.html")
