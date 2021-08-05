e30b85f16878144864586634846265d2d7c1c017
Sebastian Jeong <sebastianrcnt@gmail.com>
2021-08-05 23:12:08 +0000

    [feat] configure ssl/http

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from server.app import app
+import ssl
+
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", port=5000, threaded=True, debug=True)
+  ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS)
+  ssl_context.load_cert_chain(certfile="ssl/certificate.crt", keyfile='ssl/private.key')
+  app.run(host="0.0.0.0", port=5000, threaded=True, debug=True, ssl_context=ssl_context)
